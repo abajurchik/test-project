5bd3e2284a5db1a437324cbb00ad7caef93b5e55
Oleksii Levkov <olelevkov@ukr.net>
2018-04-25 15:23:20 +0300

    добавив ще карточки знизу

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
-    $('#analog-products__slider').owlCarousel({
+   $('#analog-products__slider').owlCarousel({
         items:5,
         loop:true,
         autoplayTimeout:2000,
         nav:true,
         pagination : false
     });
+
+        $('#analog-products__slider3').owlCarousel({
+                items:5,
+                loop:true,
+                autoplayTimeout:2000,
+                nav:true,
+                margin: 2,
+                pagination : false
+        });
+
+
 });
