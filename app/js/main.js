6224b5873eb8a9978c6105d15c4c4eba27b4f5cd
Oleksii Levkov <olelevkov@ukr.net>
2018-04-26 15:02:03 +0300

    26.04

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,48 @@
 $(document).ready(function(){
-   $('#analog-products__slider').owlCarousel({
+
+    //carousel
+
+    $('#analog-products__slider').owlCarousel({
         items:5,
         loop:true,
         autoplayTimeout:2000,
         nav:true,
         pagination : false
     });
+   $('#analog-products__slider3').owlCarousel({
+        items:5,
+        loop:true,
+        autoplayTimeout:2000,
+        nav:true,
+        margin: 2,
+        pagination : false
+    });
 
-        $('#analog-products__slider3').owlCarousel({
-                items:5,
-                loop:true,
-                autoplayTimeout:2000,
-                nav:true,
-                margin: 2,
-                pagination : false
-        });
+    //end carousel
+
+    //mix
+
+    $('#filter-project').mixItUp({
+        load: {
+            filter: '.characteristic-filter'
+        }
+    });
+
+    //end mix
+
+    // FancyBox - galery
+    $(".fancybox").fancybox({
+        // Default - with fix from scroll to top
+        helpers: {
+            overlay: {
+                locked: false
+            }
+        }
+    });
+    // End of FancyBox - galery
+
+
+
 
 
 });
